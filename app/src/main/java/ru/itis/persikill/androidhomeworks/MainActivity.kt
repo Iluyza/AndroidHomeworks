cd2401bb1bb95850b0032fbb41af616178fff666
Iluyza Asylgareeva <asylgareeva03@inbox.ru>
2022-09-27 10:49:17 +0300

    fixed First Homework

--- app/src/main/java/ru/itis/persikill/androidhomeworks/MainActivity.kt
+++ app/src/main/java/ru/itis/persikill/androidhomeworks/MainActivity.kt
@@ -1,37 +1,26 @@
 package ru.itis.persikill.androidhomeworks
 
-import android.app.Activity
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
-import android.widget.Toast
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//    fun cleanPhoto(view: View) {
-//        view.setOnClickListener() {
-//            var v : View = findViewById(R.id.iv_main)
-//            v.setVisibility(v.GONE)
-//        }
-//    }
+        val mButton1 = findViewById<EditText>(R.id.et_red)
+        val mButton2: TextView = findViewById(R.id.tv_status)
 
-        val mButton1 = findViewById<Button>(R.id.btn_red)
-        val mButton2: ImageView = findViewById(R.id.iv_main)
-
-        var temp = false
+        val temp = false
         mButton1.setOnClickListener {
             if (!temp) {
-                mButton2.visibility = View.INVISIBLE
-            } else {
-                mButton2.visibility = View.VISIBLE
+                mButton2.visibility = View.GONE
             }
-            temp = !temp
         }
     }
 }
